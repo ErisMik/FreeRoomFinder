5d5fc0d4a2bf28ded283134f5505d307baad5205
Peter Fajner <fajner1+Github@gmail.com>
2017-10-20 16:28:53 -0300

    Add refresh caller

--- Server/FreeRoomFinderServer/views.py
+++ Server/FreeRoomFinderServer/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from scraper.scrape import Scrape
 
-# Create your views here.
+
+def main(request):
+    Scrape.register_all_subjects_in_semester(year=2017, semester="Fall", campus="Halifax")
+    return "Success"
