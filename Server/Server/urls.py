8ce90f841c4a5aed6ba5556da4d7dbeff23224ef
Peter Fajner <fajner1+Github@gmail.com>
2017-10-20 16:29:10 -0300

    Add refresh caller

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from FreeRoomFinderServer import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'refresh', views.main)
 ]
