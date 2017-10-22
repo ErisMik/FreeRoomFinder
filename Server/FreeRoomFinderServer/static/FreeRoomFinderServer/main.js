3819dc5880acffc1105e240b02faae2a3936239f
Peter Fajner <mail@pfaj.ca>
2017-10-22 14:13:49 -0300

    Make client display data property, albeit in a messy manner

--- Server/FreeRoomFinderServer/static/FreeRoomFinderServer/main.js
+++ Server/FreeRoomFinderServer/static/FreeRoomFinderServer/main.js
@@ -1,40 +1,75 @@
+var allBookingValues = [];
+var bookingsList;
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/db', true);
 xhr.responseType = 'arraybuffer';
-var rooms;
-var bookings;
-var values = [];
 xhr.onload = function () {
     var uInt8Array = new Uint8Array(this.response);
     var db = new SQL.Database(uInt8Array);
-    rooms = db.exec("SELECT * FROM FreeRoomFinderServer_room");
-    bookings = db.exec("SELECT * FROM FreeRoomFinderServer_roombookedslot");
+    // set year
+    var year = new Date().getFullYear();
+    // set semester
+    var month = new Date().getMonth();
+    var semester;
+    if (month <= 4)
+        semester = "Winter";
+    else if (month <= 8)
+        semester = "Summer";
+    else
+        semester = "Fall";
+    console.log(semester);
+    // get rooms and bookings from DB
+    var rooms = db.exec("SELECT * FROM FreeRoomFinderServer_room");
+    var bookings = db.exec("SELECT * FROM FreeRoomFinderServer_roombookedslot WHERE year='" + year + "' AND semester='" + semester + "'");
+    // create booking list data
     for (var i = 0; i < bookings[0].values.length; i++) {
         var b = bookings[0].values[i];
-        values.push({
+        var room_raw = rooms[0].values[b[7]];
+        if (room_raw == undefined)
+            continue;
+        var room = room_raw[1] + " " + room_raw[2] + " " + room_raw[3];
+        allBookingValues.push({
             year: b[1],
             semester: b[2],
             weekday: b[3],
             starttime: b[4],
             endtime: b[5],
             subject: b[6],
-            room: rooms[0].values[b[7]],
+            room: room,
         });
     }
-    console.log(bookings);
+    // set up sorting
+    var options = {
+        valueNames: ["room", "subject", "starttime", "endtime", "weekday", "year", "semester"],
+        item: '<li><h3 class="room"></h3><p class="subject"</p><p class="starttime"></p><p class="endtime"></p></li>'
+    };
+    bookingsList = new List('bookinglist', options, []);
+    refreshList();
 };
 xhr.send();
-$('#search').keyup(_.debounce(onSearch, 500));
-function onSearch() {
+$("#day").change(refreshList);
+$("#time").change(refreshList);
+function refreshList() {
+    // get time in HHMM format
+    var time_split = $("#time").val().split(":");
+    var time = parseInt(time_split[0] + "" + time_split[1]);
+    // only show bookings for the current weekday and current time
+    var values = [];
+    var weekday = $("#day").val();
+    bookingsList.clear();
+    for (var i = 0; i < allBookingValues.length; i++) {
+        var booking = allBookingValues[i];
+        var starttime_split = booking.starttime.split(":");
+        var starttime = parseInt(starttime_split[0] + "" + starttime_split[1]);
+        var endtime_split = booking.endtime.split(":");
+        var endtime = parseInt(endtime_split[0] + "" + endtime_split[1]);
+        if (allBookingValues[i].weekday == weekday /*&& starttime <= time && time <= endtime*/) {
+            bookingsList.add(allBookingValues[i]);
+        }
+    }
 }
 // set up time picker
 $('#time').timepicker({ timeFormat: "H:i" });
 $('#timenow').on('click', function () {
     $('#time').timepicker('setTime', new Date());
 });
-// set up sorting
-var options = {
-    valueNames: ["room", "subject", "starttime", "endtime", "weekday", "year", "semester"],
-    item: '<li><h3 class="room"></h3><p class="subject"</p><p class="year"></p><p class="semester"></p><p class="starttime"></p><p class="endtime"></p><p class="weekday"></p></li>'
-};
-var userList = new List('bookinglist', options, values);
--- Server/FreeRoomFinderServer/static/FreeRoomFinderServer/main.ts
+++ Server/FreeRoomFinderServer/static/FreeRoomFinderServer/main.ts
@@ -1,37 +1,81 @@
+let allBookingValues = [];
+let bookingsList;
+
 let xhr = new XMLHttpRequest();
 xhr.open('GET', '/db', true);
 xhr.responseType = 'arraybuffer';
-
-let rooms;
-let bookings;
-let values = []
-
 xhr.onload = function () {
     let uInt8Array = new Uint8Array(this.response);
     let db = new SQL.Database(uInt8Array);
-    rooms = db.exec("SELECT * FROM FreeRoomFinderServer_room");
-    bookings = db.exec("SELECT * FROM FreeRoomFinderServer_roombookedslot");
+
+    // set year
+    let year = new Date().getFullYear();
+
+    // set semester
+    let month = new Date().getMonth();
+    let semester;
+    if (month <= 4) semester = "Winter";
+    else if (month <= 8) semester = "Summer";
+    else semester = "Fall";
+    console.log(semester);
+
+    // get rooms and bookings from DB
+    let rooms = db.exec("SELECT * FROM FreeRoomFinderServer_room");
+    let bookings = db.exec(`SELECT * FROM FreeRoomFinderServer_roombookedslot WHERE year='${year}' AND semester='${semester}'`);
+
+    // create booking list data
     for (let i = 0; i < bookings[0].values.length; i++) {
         let b = bookings[0].values[i];
-        values.push({
+        let room_raw = rooms[0].values[b[7]];
+        if (room_raw == undefined) continue;
+        let room = `${room_raw[1]} ${room_raw[2]} ${room_raw[3]}`;
+        allBookingValues.push({
             year: b[1],
             semester: b[2],
             weekday: b[3],
             starttime: b[4],
             endtime: b[5],
             subject: b[6],
-            room: rooms[0].values[b[7]],
+            room: room,
         })
     }
 
-    console.log(bookings)
+    // set up sorting
+    let options = {
+        valueNames: ["room", "subject", "starttime", "endtime", "weekday", "year", "semester"],
+        item: '<li><h3 class="room"></h3><p class="subject"</p><p class="starttime"></p><p class="endtime"></p></li>'
+    };
+
+    bookingsList = new List('bookinglist', options, []);
+
+    refreshList();
 };
+
 xhr.send();
 
-$('#search').keyup(_.debounce(onSearch, 500));
+$("#day").change(refreshList);
+$("#time").change(refreshList);
 
-function onSearch() {
 
+function refreshList() {
+    // get time in HHMM format
+    let time_split = $("#time").val().split(":");
+    let time = parseInt(time_split[0] + "" + time_split[1]);
+
+    // only show bookings for the current weekday and current time
+    let values = [];
+    let weekday = $("#day").val();
+    bookingsList.clear();
+    for (let i = 0; i < allBookingValues.length; i++) {
+        let booking = allBookingValues[i];
+        let starttime_split = booking.starttime.split(":");
+        let starttime = parseInt(starttime_split[0] + "" + starttime_split[1]);
+        let endtime_split = booking.endtime.split(":");
+        let endtime = parseInt(endtime_split[0] + "" + endtime_split[1]);
+        if (allBookingValues[i].weekday == weekday /*&& starttime <= time && time <= endtime*/){
+            bookingsList.add(allBookingValues[i]);
+        }
+    }
 }
 
 // set up time picker
@@ -39,12 +83,3 @@ $('#time').timepicker({timeFormat: "H:i"});
 $('#timenow').on('click', function () {
     $('#time').timepicker('setTime', new Date());
 });
-
-
-// set up sorting
-let options = {
-    valueNames: ["room", "subject", "starttime", "endtime", "weekday", "year", "semester"],
-    item: '<li><h3 class="room"></h3><p class="subject"</p><p class="year"></p><p class="semester"></p><p class="starttime"></p><p class="endtime"></p><p class="weekday"></p></li>'
-};
-
-let userList = new List('bookinglist', options, values);
