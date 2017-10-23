abafcc59817d6a755d009e85ae27abb816cfb9e6
Peter Fajner <fajner1+Github@gmail.com>
2017-10-23 14:35:41 -0300

    Create booking cache model

--- Server/FreeRoomFinderServer/models.py
+++ Server/FreeRoomFinderServer/models.py
@@ -27,6 +27,19 @@ class RoomBookedSlot(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
+class BookingCache(models.Model):
+    """
+    A text cache of bookings. Rooms are text, and bookings for the same room and timeslot are combined into one.
+    """
+    year = models.IntegerField(default=0)  # no sense using a date here
+    semester = models.CharField(max_length=10)  # "Fall", "Winter", or "Summer"
+    weekday = models.CharField(max_length=10)  # "Monday", "Tuesday", etc
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    occasions = models.CharField(max_length=10000)  # typically a course name and number, but leave it broad for other things
+    room = models.CharField(max_length=1000)
+
+
 class Tag(models.Model):
     """
     A tag for a room. For example, rooms in the Life Sciences Centre may also be tagged LSC for easier searchability
